75b63fed9554fc72ba1f5fdf47ab026e1d907b12
Rajan Kumar <rajansharmassi@gmail.com>
2022-07-17 19:39:48 +0530

    update the files

---
+++ mongotest.py
@@ -0,0 +1,20 @@
+import pymongo
+
+
+client = pymongo.MongoClient("mongodb+srv://root:root@rajancluster0.ub5nt.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+print(db)
+
+
+
+d = {
+    "name":"Rajan",
+    "email": "rajansharmassi@gmail.com",
+    "surname": "Kumar"
+
+}
+
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
